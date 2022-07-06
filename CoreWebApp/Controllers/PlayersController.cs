12f6782429d3d645078dfc9d364e9e270b43056c
Akash Verma <vermaakash60584@gmail.com>
2022-07-07 00:41:56 +0530

    adding view and controller

---
+++ CoreWebApp/Controllers/PlayersController.cs
@@ -0,0 +1,23 @@
+﻿using CoreWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CoreWebApp.Controllers
+{
+    public class PlayersController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Player> listPlayers = new List<Player>
+            {
+                new Player { PId=1,PName="MSD",PTeam="CSK",PAvg=65.89},
+                new Player { PId=2,PName="Rohit",PTeam="MI",PAvg=73.45},
+                new Player { PId=4,PName="Virat",PTeam="RCB",PAvg=78.56},
+                new Player { PId=3,PName="Vijay",PTeam="CSK",PAvg=70.26}
+
+            };
+
+            return View(listPlayers);
+        }
+    }
+}
