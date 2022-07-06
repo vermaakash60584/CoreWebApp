491dca4f8abdbad7f3c2b97628a1da0b727c2461
Akash Verma <vermaakash60584@gmail.com>
2022-07-07 00:28:08 +0530

    Adding player in Model

---
+++ CoreWebApp/Models/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace CoreWebApp.Models
+{
+    public class Player
+    {
+        public int PId { get; set; }
+        public string PName { get; set; }
+        public string PTeam { get; set; }
+        public double PAvg { get; set; }
+    }
+}
